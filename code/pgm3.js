405d4cc786254a5cbcc499b939676d420cf692d6
xonatis-academy <formation@xonatis.com>
2021-03-23 16:09:09 +0100

    Adding exercices

---
+++ code/pgm3.js
@@ -0,0 +1,18 @@
+// on crée nos variables
+let a;
+let b;
+let resultat;
+
+resultat = 'Petit';
+a = true;
+b = true;
+
+// On ajoute une condition
+if (a && b)
+{
+    resultat = 'Grand';
+}
+
+// On affiche le resultat
+console.log(resultat);
+
